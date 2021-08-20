6ce36df89733a81d15ee52ed2857d1117721d1a0
MSI02 <darkzhon@hanmail.net>
2021-08-20 18:05:30 +0900

    탭바

---
+++ FirstApp/tabbar_example/android/app/src/main/kotlin/com/akm/tabbar_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akm.tabbar_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FirstApp/tabbar_example/lib/sub/firstPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("첫 번째 페이지"),
+        ),
+      ),
+    );
+  }
+}
