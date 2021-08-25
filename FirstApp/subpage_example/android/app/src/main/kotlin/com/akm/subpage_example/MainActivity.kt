d6a4b0401da485e91f88569e4e8ca031c67ffff2
MSI02 <darkzhon@hanmail.net>
2021-08-25 17:43:32 +0900

    페이지 이동하기 구현

---
+++ FirstApp/subpage_example/android/app/src/main/kotlin/com/akm/subpage_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akm.subpage_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FirstApp/subpage_example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<FirstPage> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sub Page Main"),),
+      body: Container(
+        child: Center(
+          child: Text("첫 번째 페이지"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage()));
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second Page"),),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
