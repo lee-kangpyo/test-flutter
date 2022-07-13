e4acffb1539943ee2058956d53dae2dc692e9b80
그램 <darkzhon@hanmail.net>
2022-07-14 00:53:21 +0900

    push 관련 메소드들

--- code_factory/navigation/lib/screen/route_two_screen.dart
+++ code_factory/navigation/lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
@@ -24,10 +25,30 @@ class RouteTwoScreen extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).pushNamed(
               "/three",
-              arguments: 999,
+              arguments: "pushNamed",
             );
           },
           child: Text("Push named"),
+        ),
+
+        //replace 2를 3로 대체 하므로 3에서 pop() 할경우 1로 돌아간다.
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed(
+              "/three",
+              arguments: "pushReplacementNamed",
+            );
+          },
+          child: Text("Push Replacement Named"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              "/three",
+              (route) => route.settings.name == "/",
+            );
+          },
+          child: Text("Push Named And Remove Until"),
         ),
       ],
     );
