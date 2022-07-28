ef0dcc25b19a1a5f4e6e775ccd447840e4787311
그램 <darkzhon@hanmail.net>
2022-07-29 00:50:40 +0900

    달력 디자인하기

--- code_factory/calendar_scheduler/lib/component/calendar.dart
+++ code_factory/calendar_scheduler/lib/component/calendar.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,20 +14,46 @@ class _CalendarState extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
+    final defaultBoxDeco = BoxDecoration(
+      borderRadius: BorderRadius.circular(6.0),
+      color: Colors.grey[200],
+    );
+
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
     return TableCalendar(
       focusedDay: DateTime.now(),
       firstDay: DateTime(1800),
       lastDay: DateTime(3000),
       headerStyle: const HeaderStyle(
-        formatButtonVisible: false,
-        titleCentered: true,
-        titleTextStyle: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 16.0,
-        )
+          formatButtonVisible: false,
+          titleCentered: true,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 16.0,
+          )),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false, // 오늘날짜 하일라이트 여부
+        defaultDecoration: defaultBoxDeco,
+        weekendDecoration: defaultBoxDeco.copyWith(color: Colors.yellow[500]),
+        selectedDecoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6.0),
+            border: Border.all(
+              color: PRIMARY_COLOR,
+              width: 1.0,
+            )),
+        defaultTextStyle: defaultTextStyle,
+        weekendTextStyle: defaultTextStyle.copyWith(color: Colors.red[300]),
+        selectedTextStyle: defaultTextStyle.copyWith(
+          color: PRIMARY_COLOR,
+        ),
       ),
       //캘린더의 날짜를 클릭했을때 실행이되는 함수
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay){
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
         print(selectedDay);
         setState(() {
           this.selectedDay = selectedDay;
@@ -35,12 +62,12 @@ class _CalendarState extends State<Calendar> {
       // onDaySelected에서 setState를 호출해서 캘린더를 다시 빌드할때
       // 현재 화면에 떠있는 모든 날짜를 대상으로 선택된 날짜 검증이 이루어진다.
       selectedDayPredicate: (DateTime date) {
-        if(selectedDay == null){
+        if (selectedDay == null) {
           return false;
         }
         return date.year == selectedDay!.year &&
-        date.month == selectedDay!.month &&
-        date.day == selectedDay!.day;
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
       },
     );
   }
