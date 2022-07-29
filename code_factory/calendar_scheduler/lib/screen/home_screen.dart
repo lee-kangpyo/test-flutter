2f2829ad9e4425b0043ab5201fe0b98637644f28
MSI02 <darkzhon@hanmail.net>
2022-07-29 17:35:02 +0900

    스케쥴 카드 1개 설계

--- code_factory/calendar_scheduler/lib/screen/home_screen.dart
+++ code_factory/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -30,12 +31,20 @@ class _HomeScreenState extends State<HomeScreen> {
               focusedDay: focusedDay,
               onDaySelected: onDaySelected,
             ),
-            const SizedBox(
-              height: 8.0,
-            ),
+            const SizedBox(height: 8.0),
             TodayBanner(
               selectedDay: selectedDay,
               scheduleCount: 4,
+            ),
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ScheduleCard(
+                startTime: 9,
+                endTime: 12,
+                content: '프로그래밍 공부하기',
+                color: Colors.red,
+              ),
             ),
           ],
         ),
